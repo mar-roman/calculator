d8dd892c360f570fe8dab6ebb2a4901c8df9f875
mar-roman <roman.markaryants@yandex.ru>
2021-07-31 10:56:32 +0300

    fix lesson02

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-let money = 100000, 
-income = '60000', 
-addExpenses = '600, 3000, 5000',
+let money = 90000,
+income = 'фриланс', 
+addExpenses = 'интернет, такси, коммуналка',
 deposit = true,
 mission = 1000000, 
-period = 10;
+period = 12;
 
 console.log(typeof money);
 console.log(typeof income);
 console.log(typeof deposit);
+console.log(addExpenses.length);
 console.log('Период равен ' + period + ' месяцев');
 console.log('Цель заработать ' + mission + ' рублей');
 console.log(addExpenses.toLowerCase().split(', '));
 
-let budgetDay = parseFloat(income) / 30;
+let budgetDay = parseFloat(money) / 30;
 console.log(budgetDay);
